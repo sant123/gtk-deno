759cf1f440d74b644b142c33b19b098c151b8d9c
Santiago Aguilar Hernandez <santiago.aguilar.821@gmail.com>
2025-09-27 18:59:23 -0500

    - exported lib and version from default import - updated README examples, closes #42

--- lib/mod.ts
+++ lib/mod.ts
@@ -6,6 +6,12 @@ const libPath: string = resolveGtkLibrary();
 /** Current GTK version */
 const version: GtkVersion = getVersion(libPath);
 
+if (import.meta.main) {
+  const { micro, major, minor } = version;
+  console.log(`Gtk version: ${major}.${minor}.${micro}`);
+}
+
+/** Gtk with all symbols registered */
 const lib = Deno.dlopen(libPath, symbols);
 
 export { lib, version };
