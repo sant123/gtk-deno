fc20778bf6e0e7fa95a2ebc53ed547e57b6ccd4f
Santiago Aguilar Hernandez <santiago.aguilar.821@gmail.com>
2025-09-30 14:46:05 -0500

    - added signals to GtkApplication

--- src/GtkApplication/events.ts
+++ src/GtkApplication/events.ts
@@ -2,6 +2,8 @@ import type { CallbacksFromDefs } from "types";
 
 export const ffiDefinitions = {
   activate: { parameters: ["pointer", "pointer"], result: "void" },
+  "window-added": { parameters: ["pointer", "pointer", "pointer"], result: "void" },
+  "window-removed": { parameters: ["pointer", "pointer", "pointer"], result: "void" },
 } as const satisfies Record<string, Deno.UnsafeCallbackDefinition>;
 
 export type Signals = keyof typeof ffiDefinitions;
