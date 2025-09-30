59ba174aacb68f1a38f095630288d9875d478820
Santiago Aguilar Hernandez <santiago.aguilar.821@gmail.com>
2025-09-30 14:58:22 -0500

    - created GtkApplication example

--- examples/GtkApplication/GtkApplication.ts
+++ examples/GtkApplication/GtkApplication.ts
@@ -2,7 +2,7 @@ import { GtkApplication, GtkApplicationFlags } from "@onyx/gtk/GtkApplication";
 import { GtkApplicationWindow } from "@onyx/gtk/GtkApplicationWindow";
 
 const app = new GtkApplication(
-  "org.gtk.deno.example",
+  "org.gtk.example",
   GtkApplicationFlags.G_APPLICATION_DEFAULT_FLAGS,
 );
 
@@ -10,17 +10,8 @@ app.connect("activate", () => {
   const win = new GtkApplicationWindow(app);
 
   win.title = "Window";
-  win.setDefaultSize(800, 600);
+  win.setDefaultSize(200, 200);
   win.present();
-
-  console.log("Activated");
-
-  win.connect("close-request", () => {
-    console.log("Trying to close?");
-    return false;
-  });
 });
 
 app.run();
-
-// console.log(Deno[Deno.internal].core.resources());
