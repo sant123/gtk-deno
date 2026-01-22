17ce35907c9a0ee72fd4f80e85d19757dfbdb1a2
Santiago Aguilar Hernandez <santiago.aguilar.821@gmail.com>
2026-01-22 18:42:25 -0500

    - moved GtkFileDialog errors to a new file

---
+++ src/messages/GtkFileDialog.errors.ts
@@ -0,0 +1,4 @@
+export const EMPTY_DEFAULT_FILTER = "Default file filter is empty.";
+export const EMPTY_FILTERS = "One or more filters in setFilters() is empty.";
+export const DEFAULT_FILTER_NOT_EXISTS_IN_FILTERS =
+  "The default filter must be present in the filter list.";
